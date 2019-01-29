fbcc4d59972f43b5f013e945bdbf8e40c6a9ff99
Jimmy Dee <jgvdthree@gmail.com>
2019-01-29 13:20:05 -0800

    JS keys mostly working in webview_example on Android. 🎉

--- examples/webview_example/android/app/src/main/java/com/webview_example/MainApplication.java
+++ examples/webview_example/android/app/src/main/java/com/webview_example/MainApplication.java
@@ -5,9 +5,10 @@ import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 
 import com.facebook.react.ReactApplication;
+
+import io.branch.rnbranch.RNBranchModule;
 import io.branch.rnbranch.RNBranchPackage;
 
-import io.branch.referral.Branch;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -18,41 +19,48 @@ import java.util.List;
 
 public class MainApplication extends MultiDexApplication implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new RNBranchPackage()
+            );
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNBranchPackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
+
+        // Replace Branch.getAutoInstance(this); with:
+        RNBranchModule.saveApplicationContext(this);
     }
 
     @Override
-    protected String getJSMainModuleName() {
-      return "index";
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        // Most prod apps seem to use multidex, so though it's not necessary with this small app,
+        // this seems to make it a more realistic example. This has had an impact on build issues
+        // before, so probably best to match partner apps as much as possible.
+        // TODO: Shouldn't this be in onCreate? If here, should the base context be used?
+        MultiDex.install(this);
     }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-    Branch.getAutoInstance(this);
-  }
-
-  @Override
-  protected void attachBaseContext(Context base) {
-    super.attachBaseContext(base);
-    MultiDex.install(this);
-  }
 }
